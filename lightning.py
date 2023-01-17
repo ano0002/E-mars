cd13b6a560ee48a4975521d6a3f99c57525460d0
Anatole Sot <anatolesot@gmail.com>
2023-01-17 13:14:05 +0100

    Added a particle system in lightning.py

--- lightning.py
+++ lightning.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3.4
 # Setup Python ----------------------------------------------- #
-import pygame, sys, random
 
-# Setup pygame/window ---------------------------------------- #
-mainClock = pygame.time.Clock()
-from pygame.locals import *
-pygame.init()
-pygame.display.set_caption('game base')
-screen = pygame.display.set_mode((500, 500),0,32)
+import pygame
+from pygame.math import Vector2
 
 def circle_surf(radius, color):
     surf = pygame.Surface((radius * 2, radius * 2))
@@ -15,43 +10,80 @@ def circle_surf(radius, color):
     surf.set_colorkey((0, 0, 0))
     return surf
 
-# [loc, velocity, timer]
-particles = []
+class Particle:
+    def __init__(self, pos, vel, timer,color,radius,gravity,glow_radius=0):
+        self.pos = pos
+        self.vel = vel.copy()
+        self.timer = timer
+        self.color = color
+        self.radius = radius
+        self.gravity = gravity
+        self.alive = True
+        self.glow_radius = glow_radius
+    
+    def update(self):
+        self.pos += self.vel
+        self.vel[1] += self.gravity
+        self.vel[0] *= 0.9
+        
+        self.timer -= 1
+        if self.timer <= 0:
+            self.alive = False
+    
+    def draw(self,screen):
+        if self.glow_radius > 0:
+            surf = circle_surf(self.glow_radius, list(x*0.2 for x in self.color))
+            screen.blit(surf, (self.pos[0]-self.glow_radius, self.pos[1]-self.glow_radius), special_flags=pygame.BLEND_RGB_ADD)
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
-# Loop ------------------------------------------------------- #
-while True:
+class Particle_Emitter:
+    def __init__(self, rate, pos, vel, color = (255,255,255), radius = 5, gravity = 0.1,timer=50,glow_radius=0):
+        self.rate = rate
+        self.pos = pos
+        self.vel = vel
+        self.color = color
+        self.radius = radius
+        self.gravity = gravity
+        self.particles = []
+        self.frame = 0
+        self.timer = timer
+        self.glow_radius = glow_radius
+        
+    def update(self):
+        self.frame += 1
+        self.vel[0] = random.random()*4-2
+        if self.frame % self.rate == 0:
+            self.particles.append(Particle(self.pos, self.vel, self.timer, self.color, self.radius, self.gravity, self.glow_radius))
+        
+        for particle in self.particles:
+            particle.update()
+            if not particle.alive:
+                self.particles.remove(particle)
+                del particle
+    
+    def draw(self,screen):
+        for particle in self.particles:
+            particle.draw(screen)
 
-    # Background --------------------------------------------- #
-    screen.fill((0,0,0))
+if __name__ == '__main__':
+    import pygame,random
 
-    pygame.draw.rect(screen, (50, 20, 120), pygame.Rect(100, 100, 200, 80))
+# Setup pygame/window ---------------------------------------- #
+    mainClock = pygame.time.Clock()
+    from pygame.locals import *
+    pygame.init()
+    pygame.display.set_caption('game base')
+    screen = pygame.display.set_mode((500, 500),0,32)
 
-    mx, my = pygame.mouse.get_pos()
-    particles.append([[mx, my], [random.randint(0, 20) / 10 - 1, -5], random.randint(6, 11)])
-
-    for particle in particles:
-        particle[0][0] += particle[1][0]
-        particle[0][1] += particle[1][1]
-        particle[2] -= 0.1
-        particle[1][1] += 0.15
-        pygame.draw.circle(screen, (255, 255, 255), [int(particle[0][0]), int(particle[0][1])], int(particle[2]))
-
-        radius = particle[2] * 2
-        screen.blit(circle_surf(radius, (20, 20, 60)), (int(particle[0][0] - radius), int(particle[0][1] - radius)), special_flags=BLEND_RGB_ADD)
-
-        if particle[2] <= 0:
-            particles.remove(particle)
-
-    # Buttons ------------------------------------------------ #
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+    emitter = Particle_Emitter(rate = 10, pos = Vector2(250, 250), vel = Vector2(1, -5),timer = 50,glow_radius=15,color=(255,0,0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
                 pygame.quit()
-                sys.exit()
-
-    # Update ------------------------------------------------- #
-    pygame.display.update()
-    mainClock.tick(60)
+                exit()
+        emitter.update()
+        emitter.pos = pygame.mouse.get_pos()
+        screen.fill((0,0,0))
+        emitter.draw(screen)
+        pygame.display.update()
+        mainClock.tick(60)
