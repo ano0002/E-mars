6fa08a4abb2195d1c1593b1bbd58183ffc48e0fe
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-02-07 20:19:49 +0100

    Added a simple ui system

--- tower defense/tower.py
+++ tower defense/tower.py
@@ -1,4 +1,21 @@
 import pygame
+
+class Turret(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__(self)
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+class PlaceHolder(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, color=(255,255,0)):
+        super().__init__()
+        self.image = pygame.Surface([width, height], pygame.SRCALPHA)
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 if __name__ == "__main__":
     from terrain import Terrain
     pygame.init()
---
+++ tower defense/ui.py
@@ -0,0 +1,52 @@
+import pygame
+
+class Button(pygame.sprite.Sprite):
+    default_on_click = lambda self,mousepos: print("Clicked")
+    
+    def __init__(self, x, y, width, height, color=(255,255,255), text="", text_color=(0,0,0), font_size=30,on_click=None):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.text = text
+        self.text_color = text_color
+        self.font_size = font_size
+        self.font = pygame.font.SysFont("Arial", self.font_size)
+        self.text_image = self.font.render(self.text, True, self.text_color)
+        self.text_rect = self.text_image.get_rect()
+        self.text_rect.center = (self.rect.width/2, self.rect.height/2)
+        self.image.blit(self.text_image, self.text_rect)
+        if on_click:
+            self.on_click = on_click
+        else:
+            self.on_click = self.default_on_click    
+    def update(self, mousepos):
+        if self.rect.collidepoint(mousepos):
+            self.image.fill((200,200,200))
+        else:
+            self.image.fill((255,255,255))
+        self.image.blit(self.text_image, self.text_rect)
+    
+        
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption("Button")
+    display = pygame.display.set_mode((800, 600))
+    buttons = pygame.sprite.Group(Button(100, 100, 100, 50, text="Test"))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                        for button in buttons:
+                            if button.rect.collidepoint(event.pos):
+                                button.on_click(mousepos=event.pos)
+        display.fill((0,0,0))
+        buttons.update(pygame.mouse.get_pos())
+        buttons.draw(display)
+        pygame.display.update()
