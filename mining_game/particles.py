9f7ca79ba2b7cfdcc11778c25cc0c633516a689a
Hormone4 <117640003+Hormone4@users.noreply.github.com>
2023-04-09 10:39:04 +0200

    Added test for particle engine
    (mining game particle engine)

--- mining_game/particles.py
+++ mining_game/particles.py
@@ -47,3 +47,28 @@ class ParticleEngine:
             self.particles.append(particle)
             if len(self.particles) > self.max_particles:
                 self.particles.pop(0)
+
+
+if "__main__" == __name__ :
+    pygame.init()
+    screen = pygame.display.set_mode((500, 500))
+    clock = pygame.time.Clock()
+
+    particle_engine = ParticleEngine(50)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        screen.fill((15, 15, 15))
+
+        particle_engine.update()
+        color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        x, y = pygame.mouse.get_pos()
+        particle_engine.create_particles(x, y, 1, color, 2)   # last argument is gravity (particles will go down more, default is 3)
+        particle_engine.draw(screen)
+
+        pygame.display.update()
+        clock.tick(60)
