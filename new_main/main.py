e1df87bb51332ea0b843536d6a7bb4414745632b
Anatole Sot <anatolesot@gmail.com>
2023-04-17 14:12:32 +0200

    Displayed a screen

---
+++ new_main/main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+#Create a screen of 1920x1080 in fullscreen mode
+screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
+
+playing = True
+
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+    pygame.display.flip()
