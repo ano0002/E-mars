cd4d5eaae6b81594f80cb1e2a590a4061d87ba92
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2023-05-22 22:54:24 +0200

    EEEUUUUHHH

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from wall_breaker.run import wallbreaker
 #from tower_defense.main import Game as TowerDefense
 pygame.init()
 
+pygame.font.init
 
 pygame.display.set_caption('E-Mars')
 
@@ -55,18 +56,27 @@ music = pygame.mixer.music.load('./music/Vast Surroundings (LOOP).mp3')
 
 music = pygame.mixer.music.play(-1)
 
+consolas = pygame.font.SysFont('Consolas', 30)
 
+consolas_bold = pygame.font.SysFont('Consolas', 31)
+consolas_bold.bold = True
 
 # menu loop  
 padding = 15
-text = pygame.font.SysFont('Consolas', 30).render('Press space to start', False, (255, 255, 255))
+text = consolas.render('Press space to start', True, (255, 255, 255))
 text_rect=text.get_rect(center=(display_width/2, display_height/2+25))
+
 button_rect = text.get_rect(center=(display_width/2, display_height/2+25))
 button_rect.x -= padding
 button_rect.y -= padding
 button_rect.width += padding*2
 button_rect.height += padding*2
 button_pressed = False
+
+text_welcome = consolas.render('Welcome to E-Mars', True, (255, 255, 255))
+
+text_welcome2 = consolas_bold.render('Welcome to E-Mars', True, (000, 000, 000))
+
 
 while not button_pressed:
     for event in pygame.event.get():
@@ -86,12 +96,10 @@ while not button_pressed:
     playing_area.show_map(display)
 
     # Draw button
-    button_color = (255, 255, 255) if button_rect.collidepoint (pygame.mouse.get_pos()) else (190, 190, 190)
+    button_color = (255, 255, 255) if button_rect.collidepoint(pygame.mouse.get_pos()) else (190, 190, 190)
     pygame.draw.rect(display, button_color, button_rect, 4)
 
     # Display text  
-    text_welcome2 = pygame.font.match_font('Consolas', 35).render('Welcome to E-Mars', False, (000, 000, 000))
-    text_welcome = pygame.font.SysFont('Consolas', 30).render('Welcome to E-Mars', False, (255, 255, 255))
     display.blit(text_welcome2,(display_width/2-text_welcome2.get_width()/2,display_height/2-62))
     display.blit(text_welcome,(display_width/2-text_welcome.get_width()/2,display_height/2-60))
     display.blit(text, text_rect)
